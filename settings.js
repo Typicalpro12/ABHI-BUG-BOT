cae987759b6107c02a3854e22e720cd1aedc6fb8
Typicalpro12 <alagbeoluwaseun14@gmail.com>
2024-08-10 04:55:06 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "919074692450"
-global.ownername = "Abhishek Suresh"
+global.ownernomer = "7026537089"
+global.ownername = "INFINITEEDITS"
 global.ytname = "YT: Comedy Melody CH"
 global.socialm = "GitHub: DGXeon"
 global.location = "India, Kerala, Idukki"
 
-global.ownernumber = '919074692450'  //creator number
+global.ownernumber = '7026537089'  //creator number
 global.ownername = 'Abhishek Suresh' //owner name
 global.botname = '𝙰𝙱𝙷𝙸-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
 
